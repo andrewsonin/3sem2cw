ba74310a0701a64a4a818679f506d97218569a3c
Andrew Sonin <sonin.cel@gmail.com>
2016-12-08 19:06:49 +0300

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,10 @@
+def generation(length=0, nils=0, sequence=''):
+    if nils == 0:
+        print(sequence + '1' * length)
+    for i in range(length):
+        string = sequence + ('1' * length)[:i] + '0'
+        generation(length - i - 1, nils - 1, string)
+A = list(map(int, input().split()))
+a = A[0]
+b = A[1]
+generation(a, a - b)
